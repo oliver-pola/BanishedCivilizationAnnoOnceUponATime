4c02886147691639ea8f69a0ca29b4126ccd36ee
Oliver Pola <5pola@informatik.uni-hamburg.de>
2020-05-04 19:01:26 +0200

    mouse movement pans camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float SceneMaxX { get; private set; }
+    public float SceneMinX { get; private set; }
+    public float SceneMaxZ { get; private set; }
+    public float SceneMinZ { get; private set; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneMaxX = 100f;
+        SceneMinX = -100f;
+        SceneMaxZ = 100f;
+        SceneMinZ = -100f;
     }
 
     // Update is called once per frame
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+    public float mouseSensitivity = 100f;
+
+    private GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameObject.FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1)) // right button
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            if (Camera.main != null)
+            {
+                Camera cam = Camera.main;
+                Vector3 pos = cam.transform.position;
+                pos.x = Mathf.Clamp(pos.x + mouseX, gm.SceneMinX, gm.SceneMaxX);
+                pos.z = Mathf.Clamp(pos.z + mouseY, gm.SceneMinZ, gm.SceneMaxZ);
+                cam.transform.position = pos;
+            }
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }

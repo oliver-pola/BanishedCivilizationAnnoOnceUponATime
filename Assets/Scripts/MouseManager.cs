308412fad9a50963f62200a5aeb2eca12528453a
Oliver Pola <5pola@informatik.uni-hamburg.de>
2020-05-04 20:09:28 +0200

    zoom with mouse wheel

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 200f;
+    public float mouseWheelSensitivity = 1000f;
+    public float zoomDistanceMin = 20f;
+    public float zoomDistanceMax = 200f;
 
     private GameManager gm;
+    private Vector3 groundpos;
+    private float zoomDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
+        groundpos = new Vector3(0f, 0f, 0f);
+        zoomDistance = 100f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Camera cam = Camera.main;
+        if (cam == null) return;
+
+        // Mouse movement pans the camera in XZ
         if (Input.GetMouseButton(1)) // right button
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -24,18 +35,21 @@ public class MouseManager : MonoBehaviour
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-            if (Camera.main != null)
-            {
-                Camera cam = Camera.main;
-                Vector3 pos = cam.transform.position;
-                pos.x = Mathf.Clamp(pos.x + mouseX, gm.SceneMinX, gm.SceneMaxX);
-                pos.z = Mathf.Clamp(pos.z + mouseY, gm.SceneMinZ, gm.SceneMaxZ);
-                cam.transform.position = pos;
-            }
+            // groundpos.y always 0 (or maybe get the height of center tile later)
+            groundpos.x = Mathf.Clamp(groundpos.x + mouseX, gm.SceneMinX, gm.SceneMaxX);
+            groundpos.z = Mathf.Clamp(groundpos.z + mouseY, gm.SceneMinZ, gm.SceneMaxZ);
         }
         else
         {
             Cursor.lockState = CursorLockMode.None;
         }
+
+        // Apply the ground position
+        cam.transform.position = groundpos;
+
+        // Mouse wheel zoom in/out using translate so that rotation is considered and the ground pos focused
+        float mouseWheel = Input.GetAxis("Mouse ScrollWheel") * mouseWheelSensitivity * Time.deltaTime;
+        zoomDistance = Mathf.Clamp(zoomDistance - mouseWheel, zoomDistanceMin, zoomDistanceMax);
+        cam.transform.Translate(0f, 0f, -zoomDistance);
     }
 }

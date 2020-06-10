f49d90c8b95133ef8bde34531c29e50f726620a7
Oliver Pola <5pola@informatik.uni-hamburg.de>
2020-06-10 23:58:23 +0200

    updated event anim

--- Assets/Scripts/ResourceAnim.cs
+++ Assets/Scripts/ResourceAnim.cs
@@ -3,10 +3,23 @@
 public class ResourceAnim : MonoBehaviour
 {
     public float rotationSpeed = 360;
+    public ParticleSystem particles;
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        if (particles)
+            particles.Play();
+    }
+
+    private void OnDisable()
+    {
+        if (particles)
+            particles.Stop();
     }
 }
